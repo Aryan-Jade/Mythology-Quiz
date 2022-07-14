89c4814f308e3974069e327c2c1d75687a5c6a34
Aryan-Jade <76610755+Aryan-Jade@users.noreply.github.com>
2022-07-14 19:59:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function mahabharat(){
+    window.location = "mahabharat.html";
+}
+
+function ramayan(){
+    window.location = "ramayan.html";
+}
+
+function vishnu_puran(){
+    window.location = "vishnu_puran.html";
+}
